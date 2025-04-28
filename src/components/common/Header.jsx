b7f21d61a3e6b36956be23202a343c2280ee10bc
anjaliGhoghari <anjalighoghari79@gmail.com>
2025-04-29 01:20:20 +0530

    feat: add layout section and header component structure

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import logo from '../../assets/images/logo.png';
+
+
+function Header() {
+    return (
+        <>
+            <header className=''>
+                <div className='container m-auto'>
+                    <div className='flex justify-between text-center items-center p-6' >
+                        <a href='#'><img className=' w-100 max-w-[118px] cursor-pointer' src={logo} alt='logo' /></a>
+                        <nav>
+                            <ul className=' text-lightGray  gap-7 flex cursor-pointer'>
+                                <li>
+                                    <a href=''>How it works</a>
+                                </li>
+                                <li>
+                                    <a href=''>Contests</a>
+                                </li>
+                                <li>
+                                    <a href=''>Stats</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <a className='btn-green' >launch app</a>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        darkBg: '#060B0F',
+        lightGray: '#737373',
+        darkGreen :'#00220D',
+        perrotGreen:'#5EFB7E',
+        textGreen:'#103900',
+        },
+    },
+  },
+  plugins: [],
+}
+
