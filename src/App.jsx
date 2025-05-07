61de227cd520831a5bb322ffc6481700dd554e51
anjaliGhoghari <anjalighoghari79@gmail.com>
2025-05-07 17:19:32 +0530

    inti:mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
      
             <Layout>
               <HeroSection />
-              <HIWSection />
+              {/* <HIWSection /> */}
               <ContestFormatsSection />
               <WinnrXBotSection />
               <WaitListSection />
